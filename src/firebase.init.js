963473402e388cbd39501425dfdf53dd38597372
Talha Jobayer <talhajobayer@gmail.com>
2022-05-11 12:54:43 +0600

    some isssues fixed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,7 +7,7 @@ import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCcPVGnLvzaMSRH3uR9jhFVendGIKUn5Ik",
+  apiKey:"AIzaSyCcPVGnLvzaMSRH3uR9jhFVendGIKUn5Ik",
   authDomain:process.env.USER_REACT_authDomain,
   projectId:process.env.USER_REACT_projectId,
   storageBucket:process.env.USER_REACT_storageBucket,
